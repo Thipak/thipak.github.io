fd7fc9bf08d37c9c887f2d13e41fdd284f68a469
Thipak B <thipak01balaji@gmail.com>
2025-05-11 01:54:43 +0530

    WIP tag

--- src/components/HomeFlexIntro.vue
+++ src/components/HomeFlexIntro.vue
@@ -2,15 +2,28 @@
 </script>
 <template>
     <div class="flex-element-two">
-        <h1>Flex Element Intro</h1>
+        <p>Software Development Engineer with almost 2 years of experience in designing, developing, </p>
+            <p>and optimizing software solutions in an Agile Workspace</p>
     </div>
 </template>
 
 <style scoped>
 .flex-element-two {
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100vh;
+    justify-content: center;
+    align-items: center;
+    padding-inline: 2rem;
     background-color: #d6aaaa;
+}
+
+p {
+    text-align: center;
+    overflow-wrap: normal;
+    font-size: 1.5rem;
+    color: #333;
 }
     
 </style>
